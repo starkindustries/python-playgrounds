bc9b4d37128667a0a0fb2bc26b16477f322a6095
Zion Perez <zionperez@gmail.com>
2023-03-25 17:22:34 -0600

    Add vscode examples: better-comments and code folding regions.

---
+++ vscode-features/better-comments-extension.py
@@ -0,0 +1,20 @@
+# Sampler for the Better Comments extension
+# https://marketplace.visualstudio.com/items?itemName=aaron-bond.better-comments
+
+# TODO: this is a todo sample comment
+
+# * BOLDED TEXT: use this for something important
+
+# // STRIKETHROUGH: use this for something that no longer applies..
+
+# ! ALERT use this for something dangerous (e.g. deprecated methods)
+# @param myparam 
+def run_sample(myparam):
+    # ? this is a query comment. meant to question the code below
+    print("hello world: ", myparam)
+
+
+
+
+if __name__ == "__main__":
+    run_sample()
